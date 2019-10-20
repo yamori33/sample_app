86017c4ab6be4e16136c5b8f05e52b6c2a1277b4
yamori33 <nls3863@gmail.com>
2019-10-20 07:57:57 +0000

    Add user following

---
+++ test/integration/following_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class FollowingTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+    @user = users(:archer)
+    log_in_as(@user)
+  end
+  
+  test "following page" do
+    get following_user_path(@user)
+    assert_not @user.following.empty?
+    assert_match @user.following.count.to_s, response.body
+    @user.following.each do |user|
+      assert_select "a[href=?]", user_path(user)
+    end
+  end
+end
